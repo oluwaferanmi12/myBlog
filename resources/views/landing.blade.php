968e6b6de732ca39b8543b93e857353a67b1896f
Oluwaferanmi <feranmijogbodo@gmail.com>
2021-07-03 09:30:06 +0100

    new_commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/blog.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="landing-background">
+        <div class="landing-nav">
+            <div class="landing-nav-header">
+                Pamela
+            </div>
+            <div class="seperator"></div>
+            <ul>
+                <li><a href="#" class="active_nav">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        {{-- Showcase landing  --}}
+        <div class="landing-showcase">
+            <div class="row" style="color:white">
+                <div class="col-lg-5" style="font-size: 50px">first col</div>
+                <div class="col-lg-5">second col</div>
+            </div>
+        </div>
+    </div>
+
+    {{-- showcase content  --}}
+
+    
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return redirect('/posts');
-});
+Route::get('/', 'App\Http\Controllers\LandingController@index');
 
 Route::resource('/posts' , 'App\Http\Controllers\PostsController' );
 Auth::routes();
